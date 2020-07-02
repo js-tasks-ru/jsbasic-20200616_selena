c3b4b905d771bcf3fd9529efcac10ab2738bebc8
T <selena.tany@gmail.com>
2020-07-02 15:46:42 +0300

    task 3 from module 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,17 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+/* !!!  optimalinoe reshenie
+    return str.split('-')
+          .map( (item, index) => index==0 ? item : item[0].toUpperCase() + item.slice(1) )
+          .join ('');
+*/          
+let arr = str.split('');
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i]=="-") {
+   arr.splice(i,1);
+   arr[i] = arr[i].toUpperCase();
+  }
+}
+return arr.join('');
 }
