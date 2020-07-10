9af9cb7fb55b2cb4447646244ae0d7b1e137b9d7
T <selena.tany@gmail.com>
2020-07-11 01:30:47 +0300

    1-3 tasks of module 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+ 
+  friends.map ( name => { 
+    let li = document.createElement('li');
+    li.innerHTML = name.firstName + ' ' + name.lastName;
+    ul.appendChild(li);
+  } )
+  return document.body.insertBefore(ul, document.body.firstChild);
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,23 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+  for (var i = 0 ; i < table.rows.length; i++) {
+    
+    let rows = table.rows[i];
+
+    for (let j = 0; j < rows.cells.length; j++) {
+
+      let status = rows.cells[3].getAttribute('data-available');
+      if (status === null) {
+        rows.hidden = true;
+      }
+      else  (status === 'true') ? rows.classList.add('available')  : rows.classList.add('unavailable');   
+
+      if (rows.cells[j].innerText==='m') {rows.classList.add('male');}
+      else if (rows.cells[j].innerText==='f') {rows.classList.add('female');}
+
+      if (Number(rows.cells[1].innerText) < '18') rows.style.textDecoration = 'line-through';
+    }
+  }
 }
