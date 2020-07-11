0587daa7234f74fe221b931b007652063830d001
T <selena.tany@gmail.com>
2020-07-11 19:18:14 +0300

    1-2 tasks from module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('button');
+  const div = document.querySelector('div');
+  button.addEventListener('click', event => {
+    div.hidden = !div.hidden;
+	});
 }
